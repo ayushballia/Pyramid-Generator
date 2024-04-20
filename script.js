76dd421845617be7bc9effad3055e6df484dc175
Ayush Tiwari <ayushtiwari@live.in>
2024-04-20 20:31:16 +0530

    complete

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const character = "#";
 const count = 8;
 const rows = [];
+let inverted = false;
 
 const padRow = (rowNumber, rowCount) => {
   return (
@@ -10,44 +11,16 @@ const padRow = (rowNumber, rowCount) => {
   );
 };
 
-// for loop includes the terms "iterator", "condition", and "iteration"
-
-// The iterator is a variable you can declare specifically in your for loop
-// to control how the loop iterates or goes through your logic.
-
-// The condition of a for loop tells the loop how many times it should iterate.
-// When the condition becomes true, the loop will stop.
-
-// Your iteration statement will tell your loop what to do with the iterator after each run.
-
-// TODO: use a different type of loop
-/* for (let i = 1; i <= count; i++) {
-  // .repeat() method available to strings. This method accepts a number as an argument,
-  // specifying the number of times to repeat the target string.
-  rows.push(padRow(i, count));
-} */
-
-/* while (rows.length < count) {
-  rows.push(padRow(rows.length + 1, count));
-} */
-
-for (let i = count; i > 0; i--) {
-  rows.push(padRow(i, count));
+for (let i = 1; i <= count; i++) {
+  if (inverted) {
+    rows.unshift(padRow(i, count));
+  } else {
+    rows.push(padRow(i, count));
+  }
 }
-
-const numbers = [1, 2, 3];
-const shifted = numbers.shift();
-console.log(shifted);
-
-const unshifted = numbers.unshift(5);
-console.log(unshifted);
-
-console.log(numbers);
 
 let result = "";
 
-// a for...of loop, which iterates over each item in an iterable object
-// and temporarily assigns it to a variable.
 for (const row of rows) {
   result = result + "\n" + row;
 }
